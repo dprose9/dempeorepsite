177b9dc183a340a7f13fcb5670ecc6f157ac08f7
dprose9 <davidparkerrose@gmail.com>
2026-06-08 08:30:40 -0600

    tttoes

--- games/tictactoe/tictactoe.js
+++ games/tictactoe/tictactoe.js
@@ -1,69 +1,78 @@
 function tictactoeFunc() {
-	document.getElementById("tictactoeid").innerHTML = `<body>
-									<div style="display:flex; justify-content:center;">
-										<img src="/games/tictactoe/tttBs.png" alt=":)">
-										<img src="/games/tictactoe/tttBh.png" alt=":)">
-										<img src="/games/tictactoe/tttVs.png" alt=":)">
-										<img src="/games/tictactoe/tttBh.png" alt=":)">
-										<img src="/games/tictactoe/tttVs.png" alt=":)">
-										<img src="/games/tictactoe/tttBh.png" alt=":)">
-										<img src="/games/tictactoe/tttBs.png" alt=":)">
-									</div>
-									<div style="display:flex; justify-content:center;">	
-										<img src="/games/tictactoe/tttBv.png" alt=":)">
-										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttV.png" alt=":)">
-										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttV.png" alt=":)">
-										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttBv.png" alt=":)">
-									</div>
-									<div style="display:flex; justify-content:center;">	
-										<img src="/games/tictactoe/tttHs.png" alt=":)">
-										<img src="/games/tictactoe/tttH.png" alt=":)">
-										<img src="/games/tictactoe/tttC.png" alt=":)">
-										<img src="/games/tictactoe/tttH.png" alt=":)">
-										<img src="/games/tictactoe/tttC.png" alt=":)">
-										<img src="/games/tictactoe/tttH.png" alt=":)">
-										<img src="/games/tictactoe/tttHs.png" alt=":)">
-									</div>
-									<div style="display:flex; justify-content:center;">	
-										<img src="/games/tictactoe/tttBv.png" alt=":)">
-										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttV.png" alt=":)">
-										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttV.png" alt=":)">
-										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttBv.png" alt=":)">
-									</div>
-									<div style="display:flex; justify-content:center;">	
-										<img src="/games/tictactoe/tttHs.png" alt=":)">
-										<img src="/games/tictactoe/tttH.png" alt=":)">
-										<img src="/games/tictactoe/tttC.png" alt=":)">
-										<img src="/games/tictactoe/tttH.png" alt=":)">
-										<img src="/games/tictactoe/tttC.png" alt=":)">
-										<img src="/games/tictactoe/tttH.png" alt=":)">
-										<img src="/games/tictactoe/tttHs.png" alt=":)">
-									</div>
-									<div style="display:flex; justify-content:center;">	
-										<img src="/games/tictactoe/tttBv.png" alt=":)">
-										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttV.png" alt=":)">
-										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttV.png" alt=":)">
-										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttBv.png" alt=":)">
-									</div>
-									<div style="display:flex; justify-content:center;">
-										<img src="/games/tictactoe/tttBs.png" alt=":)">
-										<img src="/games/tictactoe/tttBh.png" alt=":)">
-										<img src="/games/tictactoe/tttVs.png" alt=":)">
-										<img src="/games/tictactoe/tttBh.png" alt=":)">
-										<img src="/games/tictactoe/tttVs.png" alt=":)">
-										<img src="/games/tictactoe/tttBh.png" alt=":)">
-										<img src="/games/tictactoe/tttBs.png" alt=":)">
-									</div>
-								</body>`
+	document.getElementById("tictactoeid").innerHTML = 
+		`<body>
+			<div style="display:flex; justify-content:center;">
+				<img src="/games/tictactoe/tttBs.png" alt=":)">
+				<img src="/games/tictactoe/tttBh.png" alt=":)">
+				<img src="/games/tictactoe/tttVs.png" alt=":)">
+				<img src="/games/tictactoe/tttBh.png" alt=":)">
+				<img src="/games/tictactoe/tttVs.png" alt=":)">
+				<img src="/games/tictactoe/tttBh.png" alt=":)">
+				<img src="/games/tictactoe/tttBs.png" alt=":)">
+			</div>
+			<div style="display:flex; justify-content:center;">	
+				<img src="/games/tictactoe/tttBv.png" alt=":)">
+				<span  id="ttt11">
+					<button onclick="tttOFunc()">
+						<img src="/games/tictactoe/tttB.png" alt="?">
+					</button>
+				</span>
+				<img src="/games/tictactoe/tttV.png" alt=":)">
+				<img src="/games/tictactoe/tttB.png" alt="?">
+				<img src="/games/tictactoe/tttV.png" alt=":)">
+				<img src="/games/tictactoe/tttB.png" alt="?">
+				<img src="/games/tictactoe/tttBv.png" alt=":)">
+			</div>
+			<div style="display:flex; justify-content:center;">	
+				<img src="/games/tictactoe/tttHs.png" alt=":)">
+				<img src="/games/tictactoe/tttH.png" alt=":)">
+				<img src="/games/tictactoe/tttC.png" alt=":)">
+				<img src="/games/tictactoe/tttH.png" alt=":)">
+				<img src="/games/tictactoe/tttC.png" alt=":)">
+				<img src="/games/tictactoe/tttH.png" alt=":)">
+				<img src="/games/tictactoe/tttHs.png" alt=":)">
+			</div>
+			<div style="display:flex; justify-content:center;">	
+				<img src="/games/tictactoe/tttBv.png" alt=":)">
+				<img src="/games/tictactoe/tttB.png" alt="?">
+				<img src="/games/tictactoe/tttV.png" alt=":)">
+				<img src="/games/tictactoe/tttB.png" alt="?">
+				<img src="/games/tictactoe/tttV.png" alt=":)">
+				<img src="/games/tictactoe/tttB.png" alt="?">
+				<img src="/games/tictactoe/tttBv.png" alt=":)">
+			</div>
+			<div style="display:flex; justify-content:center;">	
+				<img src="/games/tictactoe/tttHs.png" alt=":)">
+				<img src="/games/tictactoe/tttH.png" alt=":)">
+				<img src="/games/tictactoe/tttC.png" alt=":)">
+				<img src="/games/tictactoe/tttH.png" alt=":)">
+				<img src="/games/tictactoe/tttC.png" alt=":)">
+				<img src="/games/tictactoe/tttH.png" alt=":)">
+				<img src="/games/tictactoe/tttHs.png" alt=":)">
+			</div>
+			<div style="display:flex; justify-content:center;">	
+				<img src="/games/tictactoe/tttBv.png" alt=":)">
+				<img src="/games/tictactoe/tttB.png" alt="?">
+				<img src="/games/tictactoe/tttV.png" alt=":)">
+				<img src="/games/tictactoe/tttB.png" alt="?">
+				<img src="/games/tictactoe/tttV.png" alt=":)">
+				<img src="/games/tictactoe/tttB.png" alt="?">
+				<img src="/games/tictactoe/tttBv.png" alt=":)">
+			</div>
+			<div style="display:flex; justify-content:center;">
+				<img src="/games/tictactoe/tttBs.png" alt=":)">
+				<img src="/games/tictactoe/tttBh.png" alt=":)">
+				<img src="/games/tictactoe/tttVs.png" alt=":)">
+				<img src="/games/tictactoe/tttBh.png" alt=":)">
+				<img src="/games/tictactoe/tttVs.png" alt=":)">
+				<img src="/games/tictactoe/tttBh.png" alt=":)">
+				<img src="/games/tictactoe/tttBs.png" alt=":)">
+			</div>
+		</body>`
 								
 	document.getElementById("tictactoebutt").innerHTML = "Reset Tic Tac Toe";
+}
+
+function tttOFunc() {
+	document.getElementById("ttt11").innerHTML = `<img src="/games/tictactoe/tttO.png" alt="O">`;
 }
