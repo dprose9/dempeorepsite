45161f92f031267abe63243700f067876ac3babd
dprose9 <davidparkerrose@gmail.com>
2026-06-08 01:53:10 -0600

    tttoes

--- games/tictactoe/tictactoe.js
+++ games/tictactoe/tictactoe.js
@@ -1,31 +1,33 @@
 function tictactoeFunc() {
 	document.getElementById("tictactoeid").innerHTML = `<body>
-									<p style="display:block; text-align:center; margin:0;">
+									<div style="display:flex; text-align:center;">
 										<img src="/games/tictactoe/tttBs.png" alt=":)">
 										<img src="/games/tictactoe/tttBh.png" alt=":)">
 										<img src="/games/tictactoe/tttVs.png" alt=":)">
 										<img src="/games/tictactoe/tttBh.png" alt=":)">
 										<img src="/games/tictactoe/tttVs.png" alt=":)">
 										<img src="/games/tictactoe/tttBh.png" alt=":)">
-										<img src="/games/tictactoe/tttBs.png" alt=":)"><br>
-										
+										<img src="/games/tictactoe/tttBs.png" alt=":)">
+									</div>
+									<div style="display:flex; text-align:center;">	
 										<img src="/games/tictactoe/tttBv.png" alt=":)">
 										<img src="/games/tictactoe/tttB.png" alt="?">
 										<img src="/games/tictactoe/tttV.png" alt=":)">
 										<img src="/games/tictactoe/tttB.png" alt="?">
 										<img src="/games/tictactoe/tttV.png" alt=":)">
 										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttBv.png" alt=":)"><br>
-										
+										<img src="/games/tictactoe/tttBv.png" alt=":)">
+									</div>
+									<div style="display:flex; text-align:center;">	
 										<img src="/games/tictactoe/tttHs.png" alt=":)">
 										<img src="/games/tictactoe/tttH.png" alt=":)">
 										<img src="/games/tictactoe/tttC.png" alt=":)">
 										<img src="/games/tictactoe/tttH.png" alt=":)">
 										<img src="/games/tictactoe/tttC.png" alt=":)">
 										<img src="/games/tictactoe/tttH.png" alt=":)">
-										<img src="/games/tictactoe/tttHs.png" alt=":)"><br>
+										<img src="/games/tictactoe/tttHs.png" alt=":)">
 										
-									</p>
+									</div>
 								</body>`
 								
 	document.getElementById("tictactoebutt").innerHTML = "Reset Tic Tac Toe";
