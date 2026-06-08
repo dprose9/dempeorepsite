d19055e86dfe8b18c1bad75b2994755be2291d79
dprose9 <davidparkerrose@gmail.com>
2026-06-07 21:43:02 -0600

    gams???

--- lol.js
+++ lol.js
@@ -8,11 +8,11 @@ function boomFunc() {
 					<link rel="stylesheet" href="/format/437style.css">
 					<title>BOOM!!!!!!!</title>
 				</head>
-				<body style="background-color:#AA0000; line-height:100px;">
+				<body style="background-color:#AA0000;">
 					<p style="color:#FFFF55; background-color:#AA5500; font-size:300px; text-align:center;">
 						BOOM!
 					</p>
-					<p style="color:#FFFFFF; font-size:30px; text-align:center;">
+					<p style="color:#FFFFFF; font-size:30px; line-height:100px; text-align:center;">
 						Oops!&nbsp You 'sploded the site...&nbsp Maybe refresh and try again?
 					</p>
 				</body>
