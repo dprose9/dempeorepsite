f854a22c31473a5c7f4641aaba31c318f1ff9d8e
dprose9 <davidparkerrose@gmail.com>
2026-06-08 00:25:20 -0600

    gams???

--- lol.js
+++ lol.js
@@ -22,6 +22,18 @@ function boomFunc() {
 }
 
 function tictactoeFunc() {
-	document.getElementById("tictactoeid").innerHTML = "Tic Tac Toe Game";
+	document.getElementById("tictactoeid").innerHTML = `<body>
+									<p style="font-size:40px; text-align:center;">
+										&nbsp&nbsp╷&nbsp╷&nbsp&nbsp<br>
+										&nbsp&nbsp│&nbsp│&nbsp&nbsp<br>
+										╶─┼─┼─╴<br>
+										&nbsp&nbsp│&nbsp│&nbsp&nbsp<br>
+										╶─┼─┼─╴<br>
+										&nbsp&nbsp│&nbsp│&nbsp&nbsp<br>
+										&nbsp&nbsp╵&nbsp╵&nbsp&nbsp<br>
+										
+									</p>
+								</body>`
+								
 	document.getElementById("tictactoebutt").innerHTML = "Reset Tic Tac Toe";
 }
