0744bb97ce51b52099c78420773580a22bf7a277
dprose9 <davidparkerrose@gmail.com>
2026-06-08 16:13:20 -0600

    rubicks

--- games/rubix/rubix.js
+++ games/rubix/rubix.js
@@ -1,7 +1,7 @@
 
 function rubixInit() {
 	document.getElementById("rubixID").innerHTML =
-		`<body style="justify-content:center;">
+		`<body style="text-align:center;">
 			<style>
 				.box {
 					width: 100px;
@@ -10,8 +10,29 @@ function rubixInit() {
 					border: 8px solid #000000;
 					vertical-align: top;
 				}
+				
+				button.ll {
+					background-image:url(/games/tictactoe/tttX.png);
+					background-size:cover;
+				}
+				button.dd {
+					background-image:url(/games/tictactoe/tttXw.gif);
+				}
+				
+				button.rr {
+					background-image:url(/games/tictactoe/tttO.png);
+					background-size:cover;
+				}
+				button.uu {
+					background-image:url(/games/tictactoe/tttOw.gif);
+				}
+				button.cc {
+					background-image:url(/games/tictactoe/tttOw.gif);
+				}
+				
+				
 			</style>
-
+			
 			<div class="box" style="background-color:#AA0000;"></div><!--
 			--><div class="box" style="background-color:#AA0000;"></div><!--
 			--><div class="box" style="background-color:#AA0000;"></div><br>
