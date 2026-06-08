d79b76499d2794e67cc9606f98db6912ec9b378c
dprose9 <davidparkerrose@gmail.com>
2026-06-07 20:52:21 -0600

    gams???

--- lol.js
+++ lol.js
@@ -1,3 +1,9 @@
 function urmomFunc() {
 	document.getElementById("lolorurmom").innerHTML = "URMOM";
 }
+
+function boomFunc() {
+	document.write("<p style='font-size:100px; text-align:center;'>
+				BOOM
+			</p>")
+}
