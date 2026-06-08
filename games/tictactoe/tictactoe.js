4a93d540645801e8d3937de46929fa446354e324
dprose9 <davidparkerrose@gmail.com>
2026-06-08 01:27:36 -0600

    tttoes

--- games/tictactoe/tictactoe.js
+++ games/tictactoe/tictactoe.js
@@ -1,29 +1,29 @@
 function tictactoeFunc() {
 	document.getElementById("tictactoeid").innerHTML = `<body>
 									<div style="display:flex; text-align:center;">
-										<img src="/games/tictactoe/tttBs.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttBh.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttVs.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttBh.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttVs.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttBh.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttBs.png" alt=":)" width="50%"><br>
+										<img src="/games/tictactoe/tttBs.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttBh.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttVs.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttBh.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttVs.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttBh.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttBs.png" alt=":)" width="50%" height="50%"><br>
 										
-										<img src="/games/tictactoe/tttBv.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttB.png" alt="?" width="50%">
-										<img src="/games/tictactoe/tttV.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttB.png" alt="?" width="50%">
-										<img src="/games/tictactoe/tttV.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttB.png" alt="?" width="50%">
-										<img src="/games/tictactoe/tttBv.png" alt=":)" width="50%"><br>
+										<img src="/games/tictactoe/tttBv.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttB.png" alt="?" width="50%" height="50%">
+										<img src="/games/tictactoe/tttV.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttB.png" alt="?" width="50%" height="50%">
+										<img src="/games/tictactoe/tttV.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttB.png" alt="?" width="50%" height="50%">
+										<img src="/games/tictactoe/tttBv.png" alt=":)" width="50%" height="50%"><br>
 										
-										<img src="/games/tictactoe/tttHs.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttH.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttC.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttH.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttC.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttH.png" alt=":)" width="50%">
-										<img src="/games/tictactoe/tttHs.png" alt=":)" width="50%"><br>
+										<img src="/games/tictactoe/tttHs.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttH.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttC.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttH.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttC.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttH.png" alt=":)" width="50%" height="50%">
+										<img src="/games/tictactoe/tttHs.png" alt=":)" width="50%" height="50%"><br>
 										
 									</div>
 								</body>`
