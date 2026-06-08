a9fb5eae9a4bec4901a632ff103325464296001f
dprose9 <davidparkerrose@gmail.com>
2026-06-08 01:25:02 -0600

    tttoes

--- games/tictactoe/tictactoe.js
+++ games/tictactoe/tictactoe.js
@@ -1,31 +1,31 @@
 function tictactoeFunc() {
 	document.getElementById("tictactoeid").innerHTML = `<body>
-									<p style="text-align:center;" width="50%">
-										<img src="/games/tictactoe/tttBs.png" alt=":)">
-										<img src="/games/tictactoe/tttBh.png" alt=":)">
-										<img src="/games/tictactoe/tttVs.png" alt=":)">
-										<img src="/games/tictactoe/tttBh.png" alt=":)">
-										<img src="/games/tictactoe/tttVs.png" alt=":)">
-										<img src="/games/tictactoe/tttBh.png" alt=":)">
-										<img src="/games/tictactoe/tttBs.png" alt=":)"><br>
+									<div style="display:flex; text-align:center;">
+										<img src="/games/tictactoe/tttBs.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttBh.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttVs.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttBh.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttVs.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttBh.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttBs.png" alt=":)" width="50%"><br>
 										
-										<img src="/games/tictactoe/tttBv.png" alt=":)">
-										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttV.png" alt=":)">
-										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttV.png" alt=":)">
-										<img src="/games/tictactoe/tttB.png" alt="?">
-										<img src="/games/tictactoe/tttBv.png" alt=":)"><br>
+										<img src="/games/tictactoe/tttBv.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttB.png" alt="?" width="50%">
+										<img src="/games/tictactoe/tttV.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttB.png" alt="?" width="50%">
+										<img src="/games/tictactoe/tttV.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttB.png" alt="?" width="50%">
+										<img src="/games/tictactoe/tttBv.png" alt=":)" width="50%"><br>
 										
-										<img src="/games/tictactoe/tttHs.png" alt=":)">
-										<img src="/games/tictactoe/tttH.png" alt=":)">
-										<img src="/games/tictactoe/tttC.png" alt=":)">
-										<img src="/games/tictactoe/tttH.png" alt=":)">
-										<img src="/games/tictactoe/tttC.png" alt=":)">
-										<img src="/games/tictactoe/tttH.png" alt=":)">
-										<img src="/games/tictactoe/tttHs.png" alt=":)"><br>
+										<img src="/games/tictactoe/tttHs.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttH.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttC.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttH.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttC.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttH.png" alt=":)" width="50%">
+										<img src="/games/tictactoe/tttHs.png" alt=":)" width="50%"><br>
 										
-									</p>
+									</div>
 								</body>`
 								
 	document.getElementById("tictactoebutt").innerHTML = "Reset Tic Tac Toe";
