71fedec2d7545608a30477889934a3a58cc386e7
dprose9 <davidparkerrose@gmail.com>
2026-06-08 01:10:48 -0600

    tttoes

--- lol.js
+++ lol.js
@@ -20,20 +20,3 @@ function boomFunc() {
 				</body>
 			</html>`);
 }
-
-function tictactoeFunc() {
-	document.getElementById("tictactoeid").innerHTML = `<body>
-									<p style="font-size:40px; text-align:center;">
-										&nbsp&nbsp&nbsp&nbsp╷&nbsp&nbsp&nbsp╷&nbsp&nbsp&nbsp&nbsp<br>
-										&nbsp&nbsp&nbsp&nbsp│&nbsp&nbsp&nbsp│&nbsp&nbsp&nbsp&nbsp<br>
-										╶───┼───┼───╴<br>
-										&nbsp&nbsp&nbsp&nbsp│<img src="/games/tictactoe/tttX.png" alt="X" width="40px">│&nbsp&nbsp&nbsp&nbsp<br>
-										╶───┼───┼───╴<br>
-										&nbsp&nbsp&nbsp&nbsp│&nbsp&nbsp&nbsp│&nbsp&nbsp&nbsp&nbsp<br>
-										&nbsp&nbsp&nbsp&nbsp╵&nbsp&nbsp&nbsp╵&nbsp&nbsp&nbsp&nbsp<br>
-										
-									</p>
-								</body>`
-								
-	document.getElementById("tictactoebutt").innerHTML = "Reset Tic Tac Toe";
-}
