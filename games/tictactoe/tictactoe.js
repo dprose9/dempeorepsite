b1c48f9accbe0eb86ba9c5031b4ebd1aa0b662d2
dprose9 <davidparkerrose@gmail.com>
2026-06-08 01:57:20 -0600

    tttoes

--- games/tictactoe/tictactoe.js
+++ games/tictactoe/tictactoe.js
@@ -26,7 +26,42 @@ function tictactoeFunc() {
 										<img src="/games/tictactoe/tttC.png" alt=":)">
 										<img src="/games/tictactoe/tttH.png" alt=":)">
 										<img src="/games/tictactoe/tttHs.png" alt=":)">
-										
+									</div>
+									<div style="display:flex; justify-content:center;">	
+										<img src="/games/tictactoe/tttBv.png" alt=":)">
+										<img src="/games/tictactoe/tttB.png" alt="?">
+										<img src="/games/tictactoe/tttV.png" alt=":)">
+										<img src="/games/tictactoe/tttB.png" alt="?">
+										<img src="/games/tictactoe/tttV.png" alt=":)">
+										<img src="/games/tictactoe/tttB.png" alt="?">
+										<img src="/games/tictactoe/tttBv.png" alt=":)">
+									</div>
+									<div style="display:flex; justify-content:center;">	
+										<img src="/games/tictactoe/tttHs.png" alt=":)">
+										<img src="/games/tictactoe/tttH.png" alt=":)">
+										<img src="/games/tictactoe/tttC.png" alt=":)">
+										<img src="/games/tictactoe/tttH.png" alt=":)">
+										<img src="/games/tictactoe/tttC.png" alt=":)">
+										<img src="/games/tictactoe/tttH.png" alt=":)">
+										<img src="/games/tictactoe/tttHs.png" alt=":)">
+									</div>
+									<div style="display:flex; justify-content:center;">	
+										<img src="/games/tictactoe/tttBv.png" alt=":)">
+										<img src="/games/tictactoe/tttB.png" alt="?">
+										<img src="/games/tictactoe/tttV.png" alt=":)">
+										<img src="/games/tictactoe/tttB.png" alt="?">
+										<img src="/games/tictactoe/tttV.png" alt=":)">
+										<img src="/games/tictactoe/tttB.png" alt="?">
+										<img src="/games/tictactoe/tttBv.png" alt=":)">
+									</div>
+									<div style="display:flex; justify-content:center;">
+										<img src="/games/tictactoe/tttBs.png" alt=":)">
+										<img src="/games/tictactoe/tttBh.png" alt=":)">
+										<img src="/games/tictactoe/tttVs.png" alt=":)">
+										<img src="/games/tictactoe/tttBh.png" alt=":)">
+										<img src="/games/tictactoe/tttVs.png" alt=":)">
+										<img src="/games/tictactoe/tttBh.png" alt=":)">
+										<img src="/games/tictactoe/tttBs.png" alt=":)">
 									</div>
 								</body>`
 								
