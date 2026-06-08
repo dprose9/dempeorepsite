dce970262f41e53cae91c123c92adc1e20171a96
dprose9 <davidparkerrose@gmail.com>
2026-06-08 01:18:37 -0600

    tttoes

--- games/tictactoe/tictactoe.js
+++ games/tictactoe/tictactoe.js
@@ -1,13 +1,29 @@
 function tictactoeFunc() {
 	document.getElementById("tictactoeid").innerHTML = `<body>
-									<p style="text-align:center;">
+									<p style="text-align:center;" width="50%">
 										<img src="/games/tictactoe/tttBs.png" alt=":)">
 										<img src="/games/tictactoe/tttBh.png" alt=":)">
 										<img src="/games/tictactoe/tttVs.png" alt=":)">
 										<img src="/games/tictactoe/tttBh.png" alt=":)">
 										<img src="/games/tictactoe/tttVs.png" alt=":)">
 										<img src="/games/tictactoe/tttBh.png" alt=":)">
-										<img src="/games/tictactoe/tttBs.png" alt=":)">
+										<img src="/games/tictactoe/tttBs.png" alt=":)"><br>
+										
+										<img src="/games/tictactoe/tttBv.png" alt=":)">
+										<img src="/games/tictactoe/tttB.png" alt="?">
+										<img src="/games/tictactoe/tttV.png" alt=":)">
+										<img src="/games/tictactoe/tttB.png" alt="?">
+										<img src="/games/tictactoe/tttV.png" alt=":)">
+										<img src="/games/tictactoe/tttB.png" alt="?">
+										<img src="/games/tictactoe/tttBv.png" alt=":)"><br>
+										
+										<img src="/games/tictactoe/tttHs.png" alt=":)">
+										<img src="/games/tictactoe/tttH.png" alt=":)">
+										<img src="/games/tictactoe/tttC.png" alt=":)">
+										<img src="/games/tictactoe/tttH.png" alt=":)">
+										<img src="/games/tictactoe/tttC.png" alt=":)">
+										<img src="/games/tictactoe/tttH.png" alt=":)">
+										<img src="/games/tictactoe/tttHs.png" alt=":)"><br>
 										
 									</p>
 								</body>`
