f602a46a4f8190c1f65378b2d041535361d7a810
dprose9 <davidparkerrose@gmail.com>
2026-06-07 21:24:41 -0600

    gams???

--- lol.js
+++ lol.js
@@ -3,9 +3,9 @@ function urmomFunc() {
 }
 
 function boomFunc() {
-	document.write("<body style='background-color:#AA0000; font-family:CodePage437;'>
-				<p style='color:#FFFF55; background-color:#AA5500; font-size:400px; text-align:center;'>
+	document.write(`<body style="background-color:#AA0000; font-family:CodePage437;">
+				<p style="color:#FFFF55; background-color:#AA5500; font-size:400px; text-align:center;">
 					BOOM
 				</p>
-			</body>")
+			</body>`)
 }
