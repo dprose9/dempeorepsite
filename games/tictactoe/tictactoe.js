d8f9d39fc9f88563381a505b1df18d2fddb5794f
dprose9 <davidparkerrose@gmail.com>
2026-06-08 01:55:59 -0600

    tttoes

--- games/tictactoe/tictactoe.js
+++ games/tictactoe/tictactoe.js
@@ -1,6 +1,6 @@
 function tictactoeFunc() {
 	document.getElementById("tictactoeid").innerHTML = `<body>
-									<div style="display:flex; text-align:center;">
+									<div style="display:flex; justify-content:center;">
 										<img src="/games/tictactoe/tttBs.png" alt=":)">
 										<img src="/games/tictactoe/tttBh.png" alt=":)">
 										<img src="/games/tictactoe/tttVs.png" alt=":)">
@@ -9,7 +9,7 @@ function tictactoeFunc() {
 										<img src="/games/tictactoe/tttBh.png" alt=":)">
 										<img src="/games/tictactoe/tttBs.png" alt=":)">
 									</div>
-									<div style="display:flex; text-align:center;">	
+									<div style="display:flex; justify-content:center;">	
 										<img src="/games/tictactoe/tttBv.png" alt=":)">
 										<img src="/games/tictactoe/tttB.png" alt="?">
 										<img src="/games/tictactoe/tttV.png" alt=":)">
@@ -18,7 +18,7 @@ function tictactoeFunc() {
 										<img src="/games/tictactoe/tttB.png" alt="?">
 										<img src="/games/tictactoe/tttBv.png" alt=":)">
 									</div>
-									<div style="display:flex; text-align:center;">	
+									<div style="display:flex; justify-content:center;">	
 										<img src="/games/tictactoe/tttHs.png" alt=":)">
 										<img src="/games/tictactoe/tttH.png" alt=":)">
 										<img src="/games/tictactoe/tttC.png" alt=":)">
